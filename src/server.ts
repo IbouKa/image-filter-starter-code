7c7e263f715cd8fc1a370761b4ee52e575d3fcba
ibou_ka <ibk2gallo@gmail.com>
2022-08-28 13:28:32 +0000

    commit 2

--- src/server.ts
+++ src/server.ts
@@ -17,33 +17,30 @@ const router: Router = Router();
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  app.get('/filteredimage', async (req: Request, res: Response) => {
-    const {image_url} = req.query;
-    if (!image_url) {
-      return res.status(422).send({
-        message: `image_url query param is required`
-      })
-    }
-    const filtered_img_path = await filterImageFromURL(image_url.toString())
-    res.sendFile(filtered_img_path);
   
-  })
+  
+  //! END @TODO1
+ 
+  app.get( "/filteredimage", async(req:express.Request, res:express.Response) => {
+    let {image_url} = req.query;
+    //  1. validate the image_url query
+    if (!image_url){
+      res.status(400).send('Error: The submitted url is empty');
+    } else {
+      //  2. call filterImageFromURL(image_url) to filter the image
+      await filterImageFromURL(image_url).then( function (image_filtered_path){
+        //  3. send the resulting file in the response
+        res.sendFile(image_filtered_path, () => {  
+            // 4. deletes any files on the server on finish of the response     
+            deleteLocalFiles([image_filtered_path]);       
+        });   
+      }).catch(function(err){
+        res.status(400).send('Error:' + err + 'For some reason the image cannot be filtered.');
+      });  
+
+    }
+  });
+//! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
